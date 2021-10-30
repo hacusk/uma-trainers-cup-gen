8e2e58f09979ddb12afbd094cb8b6f154cc9837e
hacusk <5301731+hacusk@users.noreply.github.com>
2021-10-30 22:50:59 +0900

    再抽選可能にした

--- src/App.svelte
+++ src/App.svelte
@@ -2,49 +2,64 @@
 	import Tailwindcss from './Tailwindcss.svelte';
 	import races from './races.json';
 
-	// drawing race
-	const drawHost =
-		races.racecourse[Math.floor(Math.random() * races.racecourse.length)]
-	const drawRacetrack = 
-		drawHost.configurable[Math.floor(Math.random() * drawHost.configurable.length)]
-	const drawResult = []
+	type drawResult = {
+		label: string,
+		value: string
+	}
 
-	// race infomation
-	drawResult.push({label: "開催地", value: drawHost.host})
-	drawResult.push({label: "バ場", value: drawRacetrack.racetrack})
-	drawResult.push({
-		label: "距離",
-		value: drawRacetrack.distance[Math.floor(Math.random() * drawRacetrack.distance.length)]
-	})
-	drawResult.push({
-		label: "右回り・左回り",
-		value: drawRacetrack.handed[Math.floor(Math.random() * drawRacetrack.handed.length)]
-	})
-	drawResult.push({
-		label: "内回り・外回り",
-		value: drawRacetrack.course[Math.floor(Math.random() * drawRacetrack.course.length)]
-	})
+	let drawHost
+	let drawRacetrack
+	let drawResult: Array<drawResult>
 
-	// race condition
-	const condition = races.condition
-	drawResult.push({
-		label: "季節",
-		value: condition.seasons[Math.floor(Math.random() * condition.seasons.length)]
-	})
-	drawResult.push({
-		label: "天気・バ場状態",
-		value: condition.weather[Math.floor(Math.random() * condition.weather.length)]
-	})
-	drawResult.push({
-		label: "やる気",
-		value: condition.motivation[Math.floor(Math.random() * condition.motivation.length)]
-	})
+	function draw() {
+		// reset
+		drawResult = []
+
+		// drawing race
+		drawHost =
+			races.racecourse[Math.floor(Math.random() * races.racecourse.length)]
+		drawRacetrack = 
+			drawHost.configurable[Math.floor(Math.random() * drawHost.configurable.length)]
+
+		// race infomation
+		drawResult.push({label: "開催地", value: drawHost.host})
+		drawResult.push({label: "バ場", value: drawRacetrack.racetrack})
+		drawResult.push({
+			label: "距離",
+			value: drawRacetrack.distance[Math.floor(Math.random() * drawRacetrack.distance.length)]
+		})
+		drawResult.push({
+			label: "右回り・左回り",
+			value: drawRacetrack.handed[Math.floor(Math.random() * drawRacetrack.handed.length)]
+		})
+		drawResult.push({
+			label: "内回り・外回り",
+			value: drawRacetrack.course[Math.floor(Math.random() * drawRacetrack.course.length)]
+		})
+
+		// race condition
+		const condition = races.condition
+		drawResult.push({
+			label: "季節",
+			value: condition.seasons[Math.floor(Math.random() * condition.seasons.length)]
+		})
+		drawResult.push({
+			label: "天気・バ場状態",
+			value: condition.weather[Math.floor(Math.random() * condition.weather.length)]
+		})
+		drawResult.push({
+			label: "やる気",
+			value: condition.motivation[Math.floor(Math.random() * condition.motivation.length)]
+		})
+	}
+
+	draw()
 </script>
 
 <Tailwindcss />
 <main class="flex flex-col">
 	<div class="flex justify-center content-center">
-		<p class="p-4 text-xl">トレーナーズカップジェネレーター</p>
+		<p class="m-4 text-xl">トレーナーズカップジェネレーター</p>
 	</div>
 
   <div class="flex flex-wrap justify-center content-center">
@@ -55,6 +70,7 @@
 			{/each}
 		</div>
 	</div>
+	<button on:click={draw} class="m-4 p-4 w-auto">再抽選</button>
 
 	<div class="flex justify-center content-center">
 		<p class="p-4 text-sm">Copyright (c) 2021 hacusk</p>
